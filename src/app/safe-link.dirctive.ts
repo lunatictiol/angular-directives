f15e57df3bcdb9cbead488205deb5d119ad65526
“sabiq” <sabiq.a@myclnq.co.in>
2024-06-28 17:44:12 +0530

    create custom directive

--- src/app/safe-link.dirctive.ts
+++ src/app/safe-link.dirctive.ts
@@ -2,10 +2,22 @@ import { Directive } from "@angular/core";
 
 @Directive({
     selector:'a[appSafeLink]',
-    standalone:true
+    standalone:true,
+    host:{
+        '(click)':'onConfirmLeave($event)'
+    }
 })
 export class SafeLinkDirectiver{
     constructor(){
         console.log("directive")
+    }
+
+    onConfirmLeave(event:MouseEvent){
+        const wantsToLeave = window.confirm("Do you want to leave?");
+        if(wantsToLeave){
+            return;
+        } 
+        event.preventDefault();
+
     }
 }
