10e74251fb01615f979b64060bc6f341ae813c28
“sabiq” <sabiq.a@myclnq.co.in>
2024-06-30 12:36:13 +0530

    add input to custom directive

--- src/app/safe-link.dirctive.ts
+++ src/app/safe-link.dirctive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
 
 @Directive({
     selector:'a[appSafeLink]',
@@ -8,13 +8,19 @@ import { Directive } from "@angular/core";
     }
 })
 export class SafeLinkDirectiver{
+    quermParam = input("myapp",{alias:'appSafeLink'})
     constructor(){
         console.log("directive")
     }
 
     onConfirmLeave(event:MouseEvent){
+
+
         const wantsToLeave = window.confirm("Do you want to leave?");
         if(wantsToLeave){
+            const address =(event.target as HTMLAnchorElement).href;
+            
+            (event.target as HTMLAnchorElement).href= address +'?from=' +this.quermParam()
             return;
         } 
         event.preventDefault();
